71f5ec367f84cdeecf7c346b5e443b79157db7f5
jtwolfe <jamie.t.wolfe@gmail.com>
2024-12-23 13:45:26 +1000

    Update run.sh
    Signed-off-by: jtwolfe <jamie.t.wolfe@gmail.com>

--- backend/python/vall-e-x/run.sh
+++ backend/python/vall-e-x/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-BACKEND_FILE="${MY_DIR}/source/backend.py"
+BACKEND_FILE="$(dirname "$(realpath "$0")")/source/backend.py"
+# BACKEND_FILE="${MY_DIR}/source/backend.py" # refer to proper backend src
 
 source $(dirname $0)/../common/libbackend.sh
 
